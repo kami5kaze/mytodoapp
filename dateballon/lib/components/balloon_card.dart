251aded635404d02ea3060b8ac71482d6d188203
[kusoerooyajikamiyama@gmail.com] <kusoerooyajikamiyama@gmail.com>
2024-05-31 12:02:33 +0900

    Ballooncardのコンポーネント作成　一旦完了(改善等の余地あり)

--- dateballon/lib/components/balloon_card.dart
+++ dateballon/lib/components/balloon_card.dart
@@ -8,52 +8,26 @@ class BalloonCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CustomPaint(
-        painter: BallonShapePainter(),
-        child: Container(
-          width: 200,
-          height: 100,
-          alignment: Alignment.center,
-          child: Column(
-            children: [
-              Padding(
-                child: Text(title),
-                padding: EdgeInsetsDirectional.all(5),
-              ),
-              Text(time),
-            ],
+      child: Container(
+        height: 100,
+        width: 100,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/images/balloon.png'),
+            fit: BoxFit.fill,
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Text(title),
+            ),
+            Text(time),
+          ],
         ),
       ),
     );
-  }
-}
-
-//風船の描画
-class BallonShapePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()..color = Colors.white;
-
-    Path path = Path()
-      ..moveTo(size.width / 2, size.height) //bottom center
-      ..lineTo(size.width / 2 - 10, size.height - 10)
-      ..arcToPoint(
-        Offset(size.width / 2 + 10, size.height - 10),
-        radius: Radius.circular(10),
-        clockwise: false,
-      )
-      ..close();
-
-    canvas.drawPath(path, paint);
-
-    Rect rect = Rect.fromLTWH(0, 0, size.width, size.height - 10);
-    RRect rrect = RRect.fromRectAndRadius(rect, Radius.circular(20));
-    canvas.drawRRect(rrect, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
   }
 }
